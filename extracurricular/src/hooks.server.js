ba17a62d1094420fa2b346c216dbfe75046f813f
SamSmith2004 <samsmith18342@gmail.com>
2026-02-25 16:47:10 +0000

    made all routes protected by default except login and redirect to login

--- extracurricular/src/hooks.server.js
+++ extracurricular/src/hooks.server.js
@@ -1,12 +1,22 @@
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { redirect } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').Handle} */ const handleBetterAuth = async ({
 	event,
 	resolve
 }) => {
+	const PUBLIC_ROUTES = ["/login"];
 	const session = await auth.api.getSession({ headers: event.request.headers });
+
+	const isPublicRoute = PUBLIC_ROUTES.some(route => 
+        event.url.pathname.startsWith(route)
+    );
+
+    if (!session && !isPublicRoute) {
+        redirect(303, "/login");
+    }
 
 	if (session) {
 		event.locals.session = session.session;
