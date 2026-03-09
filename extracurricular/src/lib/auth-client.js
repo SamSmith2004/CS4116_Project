a94e9a5894c72b8a56aa9844cb8883886383c0be
Ilias Kourousis <23361077@studentmail.ul.ie>
2026-03-09 16:38:43 +0000

    docker hosting guide and compose.yaml upload

--- extracurricular/src/lib/auth-client.js
+++ extracurricular/src/lib/auth-client.js
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/svelte"
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:5173"
+    baseURL: 'http://fritznetwork.my.to:5173'
 });
 export const { signIn, signUp, useSession } = authClient;
