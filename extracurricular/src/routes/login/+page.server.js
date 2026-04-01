ca3bda56b912146eeb3f645e05c2bfe614efd49a
SamSmith2004 <samsmith18342@gmail.com>
2026-04-01 17:04:28 +0100

    updated singup flow to be one seperate action from login and fixed a remant of mock data

--- extracurricular/src/routes/login/+page.server.js
+++ extracurricular/src/routes/login/+page.server.js
@@ -32,48 +32,5 @@ export const actions = {
 		}
 
 		return redirect(302, '/');
-	},
-	signUpInit: async (event) => {
-		const formData = await event.request.formData();
-		const email = formData.get('email').toString();
-		const password = formData.get('password').toString();
-		const dob = formData.get('dob').toString();
-
-		if (email === null || password === null || dob === null ) { 
-			return fail(400, { message: 'All fields are required' });
-		}
-		if (!isValidEmail(email)) {
-			return fail(400, { message: 'Invalid email address'});
-		}
-		if (!isAdult(dob)) { 
-			return fail(400, { message: 'You must be 18 or older to register' });
-		}
-
-		event.cookies.set('signup_temp', JSON.stringify({ email, password, dob }), {
-			path: '/login/register-details',
-			httpOnly: true,
-			sameSite: 'strict',
-			secure: false, // INVESTING
-			maxAge: 60 * 10 // 10m
-		});
-		
-		return redirect(302, '/login/register-details');
 	}
 };
-
-const isAdult = (dob) => {
-    const birthDate = new Date(dob);
-    const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDiff = today.getMonth() - birthDate.getMonth();
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-        age--; 
-    }
-    return age >= 18;
-}
-
-
-const isValidEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
--- extracurricular/src/routes/login/register-details/+page.server.js
+++ extracurricular/src/routes/login/register-details/+page.server.js
@@ -2,15 +2,12 @@ import { fail, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/server/auth';
 import { APIError } from 'better-auth/api';
 import { db } from '$lib/server/db';
-import { userDetails, interests, universityEnum, degreeEnum } from '$lib/server/db/schema';
+import { userDetails, interests, universityEnum, degreeEnum, interestEnum } from '$lib/server/db/schema';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
-	const raw = event.cookies.get('signup_temp');
-    if (!raw) {
-        return redirect(302, '/login');
-    }
 	return {
+		allInterests: interestEnum.enumValues,
 		allUniversities: universityEnum.enumValues,
 		allDegrees: degreeEnum.enumValues
 	};
@@ -19,6 +16,9 @@ export async function load(event) {
 export const actions = {
     signUpEmail: async (event) => {
 		const formData = await event.request.formData();
+		const email = formData.get('email');
+		const password = formData.get('password');
+		const dob = formData.get('dob');
 		const fname = formData.get('fname');
 		const lname = formData.get('lname');
 		const uni = formData.get('uni');
@@ -26,10 +26,17 @@ export const actions = {
 		const gender = formData.get('gender');
 		const partnerPref = formData.get('partnerPref');
 
-		const raw = event.cookies.get('signup_temp');
-		if (!raw) return redirect(302, '/login');
-    	const { email, password, dob } = raw ? JSON.parse(raw) : {};
-		event.cookies.delete('signup_temp', { path: '/login/register-details' });
+		if (!email || !password || !dob || !fname || !lname || !uni || !degree || !gender || !partnerPref) {
+			return fail(400, { message: 'All fields are required' });
+		}
+
+		if (!isValidEmail(email)) {
+			return fail(400, { message: 'Invalid email address' });
+		}
+
+		if (!isAdult(dob)) {
+			return fail(400, { message: 'You must be 18 or older to register' });
+		}
 
 		try {
 			const res = await auth.api.signUpEmail({
@@ -74,3 +81,19 @@ export const actions = {
 		return redirect(302, '/profile');
     }
 }
+
+const isAdult = (dob) => {
+	const birthDate = new Date(dob);
+	const today = new Date();
+	let age = today.getFullYear() - birthDate.getFullYear();
+	const monthDiff = today.getMonth() - birthDate.getMonth();
+	if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+	return age >= 18;
+};
+
+const isValidEmail = (email) => {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+};
